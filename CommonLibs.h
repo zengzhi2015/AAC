b2576f10f197fe26af5f6cd3b6bf3f5126f6aa3e
Zhi Zeng <zengzhi_2012@hotmail.com>
2015-12-18 16:56:20 +0800

    Create CommonLibs.h

---
+++ CommonLibs.h
@@ -0,0 +1,34 @@
+/*
+ * CommonLib.h
+ *
+ *  Created on: 2015年12月15日
+ *      Author: ZENG
+ */
+
+#ifndef BACKGROUNDSUBTRACTION_C_CODEBOOK_AAC3_0_COMMONLIBS_H_
+#define BACKGROUNDSUBTRACTION_C_CODEBOOK_AAC3_0_COMMONLIBS_H_
+
+#include <opencv2/opencv.hpp>
+#include "opencv2/core/core.hpp"
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "cv.h"
+#include "highgui.h"
+#include "imgproc.hpp"
+#include "cxcore.h"
+#include <math.h>
+#include <iostream>
+#include <cctype>
+#include <cstring>
+#include <string>
+#include <sstream>
+#include <VTKInit.h>
+#include <list>
+#include <vector>
+#include <map>
+#include <stack>
+
+using namespace std;
+using namespace cv;
+
+#endif /* BACKGROUNDSUBTRACTION_C_CODEBOOK_AAC3_0_COMMONLIBS_H_ */
